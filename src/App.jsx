9673616c6db83aabe629884548b3a67d7a2e5416
NachoGarRen <nachogarren@campus.monlau.com>
2025-01-14 18:32:29 +0100

    components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import React from "react";
-import CVPreview from "./CVPreview"; // Asegúrate de que la ruta sea correcta
+import CVPreview from "components/CVPreview"; // Asegúrate de que la ruta sea correcta
 
 function App() {
   return (
